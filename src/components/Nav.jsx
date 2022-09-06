2e0590d0517431f25986a211c2c740dce9c1303c
kennedy7 <kennedyhillary6@gmail.com>
2022-09-06 14:25:17 +0100

    nav commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,26 @@
 import instagramLogo from '../assets/instagram_logo.png'
 const Nav = ()=>{
-    return {
-        <nav>
+    return (
+      <nav>
           <button className='logo'>
           <img src={instagramLogo} alt="logo"></img>
           </button>
           <input type="text" className="search" placeholder='search'/>
-        </nav>
-    }
+          <span className='nav-links'>
+            <button>
+              <i className='fa fa-home'></i>
+            </button>
+            <button>
+              <i className='fa fa-comment-alt'></i>
+            </button>
+            <button>
+              <i className='fa fa-compass'></i>
+            </button>
+            <button>
+              <i className='fa fa-heart'></i>
+            </button>
+          </span>
+      </nav>
+    )
 }
 export default Nav
