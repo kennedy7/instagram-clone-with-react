431958c9472d22e1212eac113466b194f9c87a36
kennedy7 <kennedyhillary6@gmail.com>
2022-09-06 15:02:22 +0100

    Bio commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Nav from './components/Nav';
+import Bio from './components/Bio';
 
 const App =()=> {
   return (
     <div className="App">
       <Nav/>
-      
+      <Bio/>
     </div>
   );
 }
--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,12 +1,23 @@
 import profileIcon from '../assets/profileIcon.svg'
 const Bio = ()=>{
+
+const editform =()=>{
+    <form className='edit-bio-form' action="">
+        <input type="text"  id='' placeholder='your name'/>
+        <input type="text" id=''  placeholder='your about'/>
+        <input type="text" id=''  placeholder='your link'/>
+    </form>
+}
     return(
         <section className="bio">
             <div className="profile-photo" role="button" title="Click the edit button">
             <img src={profileIcon} alt="profile" />
             </div>
             <div className="profile-info">
-                <p className='profile-info'> Kennedy Hillary</p>
+                <p className='name'>Kennedy Hillary</p>
+                <p className='about'>your fav node backend developer</p>
+                <button> Edit</button>
+                {editform}
             </div>
         </section>
     )
