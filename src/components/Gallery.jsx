0ddc892efc390e8563675b97a9637f9f9660b4f5
kennedy7 <kennedyhillary6@gmail.com>
2022-09-08 18:02:54 +0100

    hardcoded images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -10,6 +10,29 @@ const Gallery =()=>{
         <label htmlFor="addPhotoInput">
             <i className="add-photo-button fas fa-plus-square"></i>
         </label>
+
+        <section className='gallery'>
+            <div className="item">
+                <img src={alex} className="item-image" alt="" />
+                <button className='delete-button'>Delete</button>
+            </div>
+            <div className="item">
+                <img src={bondocks} className="item-image" alt="" />
+                <button className='delete-button'>Delete</button>
+            </div>
+            <div className="item">
+                <img src={smoke} className="item-image" alt="" />
+                <button className='delete-button'>Delete</button>
+            </div>
+            <div className="item">
+                <img src={nude} className="item-image" alt="" />
+                <button className='delete-button'>Delete</button>
+            </div>
+            <div className="item">
+                <img src={space} className="item-image" alt="" />
+                <button className='delete-button'>Delete</button>
+            </div>
+        </section>
         </>
     )
 }
